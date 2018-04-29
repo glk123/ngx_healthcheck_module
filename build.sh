8f6768e35cd843270fb3493ea5c777f191f16f83
chance <changxunzhou@qq.com>
2018-04-30 00:09:55 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #download
 version=1.14.0
-echo =======download nginx code.
-cd /home/travis/build/zhouchangxun
+echo ============download nginx code.
+cd ../ #/home/travis/build/zhouchangxun
 wget http://nginx.org/download/nginx-$version.tar.gz
 tar -zxf nginx-$version.tar.gz
 pwd;ls
 cd nginx-$version
 
-echo ======apply patch
+echo ============apply patch
 git apply ../ngx_healthcheck_module/nginx_healthcheck_for_nginx_1.14+.patch
 
 #check dependency
@@ -15,17 +15,19 @@ dpkg -l |grep libpcre3-dev
 dpkg -l|grep zlib1g-dev
 dpkg -l|grep openssl
 ls auto/
-echo ======begin build nginx
+echo ===========begin build nginx
 ./configure --with-debug \
             --with-stream \
             --add-module=../ngx_healthcheck_module
 make
 sudo make install
 
-#start nginx
+echo ===========start nginx
+cp ../ngx_healthcheck_module/nginx.conf.example /usr/local/nginx/conf/nginx.conf
 sudo /usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
 #test
 ps -ef | grep nginx
 curl localhost
+curl localhost/status
 
-echo execute finish
+echo finish!
