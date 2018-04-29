5dfb01e71549896d227b9bcb59278415b3982ed5
chance <changxunzhou@qq.com>
2018-04-29 23:31:56 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,23 @@
-cd ../nginx;
+#download
+version=1.14.0
+cd /home/travis/build/zhouchangxun
+wget http://nginx.org/download/nginx-$version.tar.gz
+tar -zxvf nginx-$version.tar.gz
+cd nginx-$version
+
+#patch code
+git apply ../ngx_healthcheck_module/nginx-stable-1.14+.patch
+
+#build
 ./auto/configure --with-debug \
             --with-stream \
             --add-module=../ngx_healthcheck_module
-make && make install
-cd -
+make
+sudo make install
+
+#start nginx
+sudo /usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
+#test
+ps -ef | grep nginx
+
+echo execute finish
