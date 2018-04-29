cce10e081eaa5d4e619459206cce52b2047bd9cf
chance <changxunzhou@qq.com>
2018-04-29 23:55:12 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,21 @@
 #download
 version=1.14.0
+echo =======download nginx code.
 cd /home/travis/build/zhouchangxun
 wget http://nginx.org/download/nginx-$version.tar.gz
 tar -zxf nginx-$version.tar.gz
 pwd;ls
 cd nginx-$version
-ls ../
-#patch code
-git apply ../ngx_healthcheck_module/nginx-stable-1.14+.patch
+
+echo ======apply patch
+git apply ../ngx_healthcheck_module/nginx_healthcheck_for_nginx_1.14+.patch
 
 #check dependency
 dpkg -l |grep libpcre3-dev
 dpkg -l|grep zlib1g-dev
 dpkg -l|grep openssl
-#build
+ls auto/
+echo ======begin build nginx
 ./auto/configure --with-debug \
             --with-stream \
             --add-module=../ngx_healthcheck_module
